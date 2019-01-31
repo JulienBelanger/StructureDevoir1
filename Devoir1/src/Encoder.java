7c1114f1fe7b06227a40e66fc8d005f06153bfd3
Julien Belanger <julesbelan@gmail.com>
2019-01-31 11:42:36 -0500

    Create MTFOmega and initial thought on structure

--- Devoir1/src/Encoder.java
+++ Devoir1/src/Encoder.java
@@ -2,5 +2,5 @@
  * Created by julienbelanger on 2019-01-31.
  */
 public class Encoder {
-    int u;
+
 }
---
+++ Devoir1/src/MTFOmega.java
@@ -0,0 +1,32 @@
+/**
+ * Created by julienbelanger on 2019-01-31.
+ */
+public class MTFOmega {
+
+    final int[] alphabet;
+    int[] to_encode;
+
+    public MTFOmega(int[] alphabet, int[] to_encode){
+        //Change direct initialization to FileReader for streaming bytes in
+
+        this.alphabet = alphabet;
+        this.to_encode = to_encode;
+    }
+
+    public static int find(int[] changeable_alphabet, int to_retrieve)
+    {// find element to_retrieve in array alphabet and retrieves its index - see course theory for more
+        int index = 0; // change to initialize only
+        return index;
+    }
+
+    // Decoding use MTFencode with original (class) alphabet
+
+    public static int[] MTFencode(int[] to_encode, int[] changeable_alphabet)
+    {
+        for (int i = 0; i < to_encode.length; i++)
+        {
+            find()
+        }
+    }
+
+}
